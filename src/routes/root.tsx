30c55e03e9ad67f079bcc8de898e76a8263de5f0
Etudiant O'clock <samantha.ty@oclock.school>
2024-01-23 11:54:22 +0100

    scroll to top, ok

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,15 +3,19 @@ import App from "../components/App/App"
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { fetchRecipes } from "../store/actions/recipes";
 import Page from "../components/Page";
+import { useLocation } from 'react-router-dom';
 
 function Root() {
 
   const dispatch = useAppDispatch();
   const isLoading = useAppSelector((state) => state.recipes.loading || state.recipes.list.length <1);
 
+  const { pathname } = useLocation();
+  
   useEffect(()=> {
     dispatch(fetchRecipes());
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <Page>
